e7634390fb90cf6ee856a0c58ec2e8bcb30ea561
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-14 12:16:40 +0530

    Added: Fourth case to check and vaidate the User's mobile Number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -5,19 +5,17 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static final Scanner scanner = new Scanner(System.in); //Scanner function declared as static
-    public static boolean validUserEmail() {
-        //As a User need to enter the valid Email ID
-        System.out.print("Enter the Email Id : ");
-        String email = scanner.next();
-        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(email);
-        boolean r = m.matches();
-
-        if (r)
-            System.out.println("Email is valid");
+    public static void validMobileNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your mobile number : ");
+        String phoneNo = scanner.nextLine();
+        String regex3 = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
+        Pattern p3 = Pattern.compile(regex3);
+        Matcher m3 = p3.matcher(phoneNo);
+        boolean r3 = m3.matches();
+        if (r3)
+            System.out.println("Phone number is Valid");
         else
-            System.out.println("Email is !Invalid");
-        return r;
+            System.out.println("Phone number is Invalid");
     }
 }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -5,10 +5,10 @@ import static com.bridgelabz.UserRegistration.scanner;
 public class UserRegistrationMain {
     public static void main(String args[]) {
 
-        System.out.println("Enter 3 : to validate User Email ");
+        System.out.println("Enter 4 : to validate User Mobile Number ");
         switch (scanner.nextInt()){
-            case 3:
-                UserRegistration.validUserEmail();
+            case 4:
+                UserRegistration.validMobileNumber();
                 break;
              }
     }

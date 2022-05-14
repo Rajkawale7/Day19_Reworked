4b0f8f9bd2e1b99d375fab1f0e9e7ecd5a97817f
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-14 11:11:03 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+    public static void main(String args[]) {
+        System.out.println("***Welcome to The User Registration Form****");
+    }
+}
